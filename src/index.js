63fb14c62d5574b0dede75e07538a9215dc48c14
mathan <mathanganjeya@outlook.com>
2021-07-02 12:29:07 +0530

    final -> deployment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./index.scss";
 import App from "./App";
 
-const TOKEN = "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";
+const TOKEN = "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";
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   cache: new InMemoryCache(),
